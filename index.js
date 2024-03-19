01a2c03e7b7e0041b584f86bb96f7421bfb3d7ff
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-03-19 12:13:46 +0300

    Functions1

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+const scuberHq = 42;
+const blockLength = 264;
+function distanceFromHqInBlocks(pickup) {
+    if(pickup >= scuberHq) {
+        return pickup - scuberHq;
+    }
+    else {
+        return scuberHq - pickup;
+    }
+
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * blockLength;
+}
+
+function distanceTravelledInFeet(start, stop) {
+    if(start > stop) {
+        return (start-stop) * blockLength;
+    }
+    else {
+        return (stop - start) * blockLength;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetDistance = distanceTravelledInFeet(start, destination);
+    if(feetDistance <= 400) {
+        return 0;
+    }
+    else if(feetDistance > 400 && feetDistance <= 2000){
+        return (feetDistance-400) *0.02;
+    }
+    else if(feetDistance > 2500) {
+        return 'cannot travel that far';
+    }
+    else {
+        return 25;
+    }
+}
